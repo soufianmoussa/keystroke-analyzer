eea756ac9165e929adb80f7969fb9d7b2fb57ce7
soufianmoussa <soufianmoussa732@gmail.com>
2025-06-10 23:58:11 +0100

    mod des champs3

--- script.js
+++ script.js
@@ -3,9 +3,24 @@ const stats = {};
 
 fields.forEach(id => {
   let startTime, keyCount = 0, text = '', freq = {};
+  let lastKey = '', repeatCount = 0, backspaceCount = 0;
 
   const el = document.getElementById(id);
   el.addEventListener('focus', () => startTime = new Date());
+
+  el.addEventListener('keydown', (e) => {
+    const key = e.key;
+
+    if (key === lastKey) {
+      repeatCount++;
+    } else {
+      lastKey = key;
+    }
+
+    if (key === "Backspace") {
+      backspaceCount++;
+    }
+  });
 
   el.addEventListener('input', (e) => {
     const char = e.data;
@@ -20,11 +35,14 @@ fields.forEach(id => {
     const endTime = new Date();
     const duration = (endTime - startTime) / 1000;
     const topKey = Object.entries(freq).sort((a, b) => b[1] - a[1])[0]?.[0] || "Aucune";
+
     stats[id] = {
       count: keyCount,
       text: id === "password" ? "*".repeat(text.length) : text,
       topKey: topKey,
-      time: duration
+      time: duration,
+      repeat: repeatCount,
+      corrections: backspaceCount
     };
   });
 });
